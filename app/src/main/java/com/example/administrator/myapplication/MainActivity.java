2d6c1c9fcd00dca099a62756f7cd3ac9efe96884
Eric <zhanpenglu777@gmail.com>
2017-08-16 22:35:18 +0800

    V1.3 增加Viewpapaer

--- app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -3,91 +3,104 @@ package com.example.administrator.myapplication;
 import android.graphics.Color;
 import android.os.Handler;
 import android.os.Message;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 import static android.R.attr.x;
 
 public class MainActivity extends AppCompatActivity {
     LineView dy2;
-    LineView line222;
     MHorizontalScrrollView mHorizontalScrollView;
-    private Handler mHandler = new Handler(){
-        @Override
-        public void handleMessage(Message msg) {
-            dy2.updateView(0, 100);
-            dy2.invalidate();
-        }
-    };
+    private View view1;
+    private View view2;
+    private View view3;
+
+    private View view4;
+    private View view5;
+    private View view6;
+    private View view7;
+    private View view8;
+    private ViewPager pager;
+    private ArrayList<View> views;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mHorizontalScrollView = (MHorizontalScrrollView) findViewById(R.id.mHorizontalScrollView);
-        mHorizontalScrollView.initData(new String[]{"必杀1","必杀2","必杀13","必杀4","必杀5","必杀6","必杀7"},null);
-//        dy2 = new LineView(this);
-//        line222 = (LineView) findViewById(R.id.line222);
-////		dy2.updateView(200, 200);
-////		mHorizontalScrollView = (MHorizontalScrrollView) findViewById(R.id.mHorizontalScrollView);
-//        mHorizontalScrollView = (MHorizontalScrrollView) findViewById(R.id.mHorizontalScrollView);
-//        LinearLayout painLayout = new LinearLayout(this);
-//        LinearLayout.LayoutParams mLP = new LinearLayout.LayoutParams(android.widget.LinearLayout.LayoutParams.MATCH_PARENT,android.widget.LinearLayout.LayoutParams.WRAP_CONTENT);
-//        painLayout.setOrientation(LinearLayout.HORIZONTAL);
-//        painLayout.setLayoutParams(mLP);
-//
-//        LinearLayout.LayoutParams textParam = new LinearLayout.LayoutParams(android.widget.LinearLayout.LayoutParams.WRAP_CONTENT,android.widget.LinearLayout.LayoutParams.WRAP_CONTENT);
-//        textParam.setMargins(50, 0, 50, 0);
-//        for(int i = 0; i < 10; i++){
-//            TextView tv = new TextView(this);
-//            tv.setText("A"+i);
-//            //必须六位
-//            tv.setTextColor(Color.parseColor("#FF0000"));
-//            tv.setTextSize(50);
-//            tv.setLayoutParams(textParam);
-//            painLayout.addView(tv);
-//        }
-//
-//        ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(
-//                ViewGroup.LayoutParams.MATCH_PARENT,
-//                ViewGroup.LayoutParams.WRAP_CONTENT);
-//        dy2.setLayoutParams(params);
-//
-//        dy2.updateView(0, 500);
-//        painLayout.addView(dy2);
+        mHorizontalScrollView.initData(new String[]{"必杀1","必杀2","必杀13","必杀4","必杀5","必杀6","必杀7","必杀8"},null);
+
+
+
+        pager = (ViewPager) findViewById(R.id.view_pager);
+        LayoutInflater inflater = getLayoutInflater();
+        view1 = inflater.inflate(R.layout.layout1, null);
+        view2 = inflater.inflate(R.layout.layout2, null);
+        view3 = inflater.inflate(R.layout.layout3, null);
+        view4 = inflater.inflate(R.layout.layout4, null);
+        view5 = inflater.inflate(R.layout.layout5, null);
+        view6 = inflater.inflate(R.layout.layout6, null);
+        view7 = inflater.inflate(R.layout.layout7, null);
+        view8 = inflater.inflate(R.layout.layout8, null);
+
+        views = new ArrayList<>();
+        views.add(view1);
+        views.add(view2);
+        views.add(view3);
+        views.add(view4);
+        views.add(view5);
+        views.add(view6);
+        views.add(view7);
+        views.add(view8);
+
+
+        pager.setAdapter(new MyPagerAdapter(views));
+
+
 
     }
 
-    float DOWNX ;
-    float MOVEX ;
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        int action = event.getAction();
-        switch (event.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                DOWNX = event.getX();
-                break;
-            case MotionEvent.ACTION_MOVE:
-                float sum = (event.getX()-DOWNX);
-                if(sum > 0){
-                    //向右
-                   // line222.updateView(DOWNX, event.getX());
-                }else{
-                    //向左
-                    //line222.updateView(event.getX(),DOWNX );
-                }
-                break;
-            case MotionEvent.ACTION_UP:
+     class MyPagerAdapter extends PagerAdapter {
 
-                break;
-            default:
-                break;
+        private final ArrayList<View> views;
+
+        public MyPagerAdapter(ArrayList<View> views) {
+            this.views = views;
         }
 
-        return true;
+        @Override
+        public int getCount() {
+            return views.size();
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position,
+                                Object object) {
+            container.removeView(views.get(position));
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            container.addView(views.get(position));
+            return views.get(position);
+        }
+
     }
+
 }
